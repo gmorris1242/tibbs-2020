36e7d227a14ba00f8bc4bfa569d1b8a200af039e
George Morris <gmorris1242@gmail.com>
2020-01-08 09:53:00 -0500

    Add preview videos to app

--- app/javascript/custom/keepAwake.js
+++ app/javascript/custom/keepAwake.js
@@ -1,5 +1,5 @@
 var http = require("http");
 setInterval(function() {
-    http.get("http://tibbs-2020.herokuapp.com");
+    http.get("http://tibbs-2020.herokuapp.com/info");
     console.log('get')
 }, 300000); // every 5 minutes (300000)
